53d1a510ca4d4b976686884fd2a2b6e50ade574b
sorz <orz@sorz.org>
2016-03-30 14:57:54 +0800

    Initial commit

---
+++ configs.example.py
@@ -0,0 +1,9 @@
+# Copy this file to `config.py`
+DEBUG = False
+SECRET_KEY='LONG RANDOM STRING'
+
+APP_KEY='TWITTER APP KEY'
+APP_SECRET='TWITTER APP SECRET'
+
+CALLBACK='http://localhost:8080/callback/'
+
---
+++ likes-counter.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+from functools import wraps
+from collections import Counter
+from flask import Flask, redirect, request, session, url_for, render_template
+from twython import Twython, TwythonError
+
+
+app = Flask(__name__)
+app.config.from_object('configs')
+
+twitter = Twython(app.config.get('APP_KEY'), app.config.get('APP_SECRET'))
+
+def token_required(authorized=True):
+    def decorator(view):
+        @wraps(view)
+        def decorated_function(*args, **kwargs):
+            if authorized and not session.get('authorized', False):
+                return redirect(url_for('auth'))
+
+            tw = Twython(app.config.get('APP_KEY'),
+                         app.config.get('APP_SECRET'),
+                         session.get('oauth_token', ''),
+                         session.get('oauth_token_secret', ''))
+            try:
+                return view(tw, *args, **kwargs)
+            except TwythonError as e:
+                if e.error_code == 401:
+                    return redirect(url_for('auth'))
+                raise e
+        return decorated_function
+    return decorator
+
+
+@app.route('/')
+def home():
+    return 'Hello, world!~'
+
+
+@app.route('/auth/')
+def auth():
+    auth = twitter.get_authentication_tokens(
+            callback_url=app.config.get('CALLBACK'))
+    session['oauth_token'] = auth['oauth_token']
+    session['oauth_token_secret'] = auth['oauth_token_secret']
+    session['authorized'] = False
+    return redirect(auth['auth_url'])
+
+
+@app.route('/callback/')
+@token_required(authorized=False)
+def callback(tw):
+    if 'denied' in request.args:
+        return 'access denied.'
+    if 'oauth_verifier' not in request.args:
+        return 'no argument found.'
+
+    token = tw.get_authorized_tokens(request.args['oauth_verifier'])
+    session['authorized'] = True
+    session['oauth_token'] = token['oauth_token']
+    session['oauth_token_secret'] = token['oauth_token_secret']
+    return redirect(url_for('count_likes'))
+
+
+@app.route('/count-likes/')
+@token_required()
+def count_likes(tw):
+    likes = tw.get_favorites(count=200)
+    users = {like['user']['id_str']: like['user'] for like in likes}
+    counter = Counter([like['user']['id_str'] for like in likes])
+    top_users = [(count, users[uid]) for uid, count in counter.most_common(15)]
+    return render_template('count-likes.html', top_users=top_users)
+
+
+if __name__ == '__main__':
+    app.run('::1', 8080)
+
